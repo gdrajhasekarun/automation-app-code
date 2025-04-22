b6cef6bba31874028d63075dcbb6645c73cc4f3a
Automater <rajhasekarun_g_d@yahoo.com>
2025-04-21 21:07:33 -0500

    Libs for app code

--- src/main/java/com/home/learning/poc/springappcode/SpringAppCodeApplication.java
+++ src/main/java/com/home/learning/poc/springappcode/SpringAppCodeApplication.java
@@ -1,11 +1,12 @@
 package com.home.learning.poc.springappcode;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.home.learning"})
+@SpringBootApplication(scanBasePackages = {"com.home.learning.poc"})
 public class SpringAppCodeApplication {
 
     public static void main(String[] args) {
